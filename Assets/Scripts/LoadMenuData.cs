f471394175b6f67667696e3d1a764300df6762be
Enrico Guerra <engue002@fh-dortmund.de>
2020-06-04 14:38:45 +0200

    Reading and writing of menu data implemented and tested

---
+++ Assets/Scripts/LoadMenuData.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using UnityEngine;
+
+public class LoadMenuData {
+
+    private MenuData m;
+    private GameObject GameObject;
+
+    public LoadMenuData(GameObject gameObject) {
+        GameObject = gameObject;
+        m = new MenuData();
+    }
+
+    public void LoadData() {
+        string path = Application.persistentDataPath + "/menuData.json";
+        string dataString = File.ReadAllText(path);
+        JsonUtility.FromJsonOverwrite(dataString, m);
+        GameObject.GetComponent<MenuData>().PlayerName = m.PlayerName;
+        GameObject.GetComponent<MenuData>().ComfortMode = m.ComfortMode;
+        GameObject.GetComponent<MenuData>().Color = m.Color;
+    }
+}
--- Assets/Scripts/MenuData.cs
+++ Assets/Scripts/MenuData.cs
@@ -10,10 +10,22 @@ public class MenuData : MonoBehaviour {
     public bool ComfortMode;
     public Color Color;
 
+    public void Start() {
+        LoadData();
+    }
+
     public void WriteData() {
         //writing menu data into a json file
         string path = Application.persistentDataPath + "/menuData.json";
-        string json = JsonUtility.ToJson(this);
+        string json = JsonUtility.ToJson(new MenuData() { PlayerName = PlayerName, ComfortMode = ComfortMode, Color = Color });
         File.WriteAllText(path, json);
+    }
+
+    public void LoadData() {
+        LoadMenuData load = new LoadMenuData(gameObject);
+        load.LoadData();
+        //string path = Application.persistentDataPath + "/menuData.json";
+        //string dataString = File.ReadAllText(path);
+        //string[] data = dataString.Split(':');
     }
 }
