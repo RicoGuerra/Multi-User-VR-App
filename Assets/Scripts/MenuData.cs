da90dd8b5b8378b9477f957723dd47f40972a1df
Enrico Guerra <engue002@fh-dortmund.de>
2020-06-04 14:41:50 +0200

    small refactoring

--- Assets/Scripts/MenuData.cs
+++ Assets/Scripts/MenuData.cs
@@ -24,8 +24,5 @@ public class MenuData : MonoBehaviour {
     public void LoadData() {
         LoadMenuData load = new LoadMenuData(gameObject);
         load.LoadData();
-        //string path = Application.persistentDataPath + "/menuData.json";
-        //string dataString = File.ReadAllText(path);
-        //string[] data = dataString.Split(':');
     }
 }
