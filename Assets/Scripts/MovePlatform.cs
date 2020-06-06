e54b0f523e7b18b4f72abbe0a433f941890c0e6c
Enrico Guerra <engue002@fh-dortmund.de>
2020-06-06 23:08:13 +0200

    Bewegende Plattform des Kugellabyrinth-Levels fertig

--- Assets/Scripts/MovePlatform.cs
+++ Assets/Scripts/MovePlatform.cs
@@ -1,36 +1,68 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class MovePlatform : MonoBehaviour {
 
-    private bool PlayerOnPlatform;
+    private bool playerOnPlatform;
     private bool floorY;
     private bool floorX;
+    private Vector3 moveVector;
+    private Vector3 posFloorX;
+    private Vector3 posFloorY;
+    private int frameCount;
+
+    private void Start() {
+        posFloorX = transform.position;
+        posFloorY = new Vector3(15, 0, 7.5f);
+        frameCount = 0;
+        moveVector = new Vector3(0, 0, 0.1f);
+    }
 
     void Update() {
-        if (!floorY) {
-            transform.position -= new Vector3(0, 0, 0.075f);
+        if (playerOnPlatform && !floorY) {
+            frameCount++;
+            if (frameCount > 180) {
+                Move();
+            }
+        } else if (floorY) {
+            Move();
         } else {
-            transform.position += new Vector3(0, 0, 0.075f);
+            frameCount = 0;
+        }
+    }
+
+    private void Move() {
+        if (transform.position.z >= posFloorX.z) {
+            floorY = false;
+            floorX = true;
+        }
+        if (transform.position.z <= posFloorY.z) {
+            floorX = false;
+            floorY = true;
+        }
+        if (floorX) {
+            transform.position -= moveVector;
+        }
+        if (floorY && !playerOnPlatform) {
+            frameCount--;
+            if (frameCount < -240)
+                transform.position += moveVector;
         }
     }
 
     private void OnTriggerEnter(Collider other) {
         if (other.tag == "Player") {
             other.transform.parent = transform;
-            PlayerOnPlatform = true;
-        }
-        if (other.name == "FloorY") {
-            floorY = true;
+            playerOnPlatform = true;
         }
     }
 
     private void OnTriggerExit(Collider other) {
         if (other.tag == "Player") {
             other.transform.parent = null;
-            PlayerOnPlatform = false;
+            playerOnPlatform = false;
         }
     }
-
 
 
 
