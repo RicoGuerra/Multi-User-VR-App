ac486c71568c5c602304db0be104c4a50456517d
Enrico Guerra <engue002@fh-dortmund.de>
2020-06-07 19:39:29 +0200

    Fading and switching camera test - RoomOne.cs created - successful test of fading in and out - successful test of camera transition - has yet to be implemented in VR (Interfaces etc.)

---
+++ Assets/Scripts/RoomOne.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class RoomOne : MonoBehaviour {
+
+    public GameObject InterfaceX;
+    public GameObject InterfaceY;
+    public GameObject TopCamera; //wird noch zu "CameraPosition o.Ä"
+    public GameObject Camera;
+    public float FadeTime;
+
+    private bool isSwitching;
+
+    //fade and camera transition testing
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.K) && TopCamera.activeInHierarchy) {
+            StartCoroutine(FadeAndSwitch(TopCamera, Camera));
+        } else if (Input.GetKeyDown(KeyCode.K) && Camera.activeInHierarchy) {
+            StartCoroutine(FadeAndSwitch(Camera, TopCamera));
+        }
+        //nur als Test. Eigentlich sobald Interface aktiviert wird
+        //und Knopf auf Controller gedrückt wird
+    }
+
+    private IEnumerator FadeAndSwitch(GameObject from, GameObject to) {
+        SteamVR_Fade.Start(Color.black, FadeTime, true);
+
+        yield return new WaitForSeconds(FadeTime);
+        from.SetActive(false);
+        to.SetActive(true);
+
+        SteamVR_Fade.Start(Color.clear, FadeTime, true);
+    }
+}
