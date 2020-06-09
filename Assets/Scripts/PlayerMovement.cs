4b0aaf62c0a1c3c104b04f63208459909fa73b57
Enrico Guerra <engue002@fh-dortmund.de>
2020-06-09 14:51:32 +0200

    Very simple and not very good locomotion created

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public SteamVR_Action_Vector2 Move;
+    public float Speed;
+
+    void Update() {
+        MoveWithHeadDirection();
+    }
+
+    private void MoveWithHeadDirection() {
+        Vector3 direction = Player.instance.hmdTransform.TransformDirection(new Vector3(Move.axis.x, Move.axis.y, 0));
+        transform.position += Speed * Time.deltaTime * Vector3.ProjectOnPlane(direction,Vector3.up);
+    }
+
+    private void MoveWithoutHeadDirection() {
+        transform.position += Speed * Time.deltaTime * new Vector3(Move.axis.x, 0, Move.axis.y);
+    }
+}
