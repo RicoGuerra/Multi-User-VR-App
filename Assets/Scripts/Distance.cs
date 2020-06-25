2e513297a3ffe9bd2b12a91bbeaaedf5bcb2eec3
Enrico Guerra <engue002@fh-dortmund.de>
2020-06-25 17:54:09 +0200

    - added distance.cs - added distance component to the pins in Tricky Pins - Freeze all rotations to keep the from falling down

---
+++ Assets/Scripts/Distance.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Distance : MonoBehaviour {
+
+    public GameObject Obj1;
+    public GameObject Obj2;
+    public float distance;
+    public float x = 3f;
+    public float z = 3f;
+    private Vector3 origin;
+    private bool safeDistance;
+    public float threshold;
+
+    // Start is called before the first frame update
+    void Start() {
+        origin = Obj2.transform.position;
+        threshold = 0.15f;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        distance = Vector3.Distance(Obj1.transform.position, Obj2.transform.position);
+        KeepDistance();
+    }
+
+    private void KeepDistance() {
+        if (distance < 3.5) {
+            safeDistance = false;
+            AwayFromPlayer();
+        } else if (distance >= 5.5f) {
+            safeDistance = true;
+            BackToOrigin();
+        }
+    }
+
+    private void BackToOrigin() {
+        if (safeDistance && Obj2.transform.position != origin) {
+            if (Obj2.transform.position.x < origin.x && Obj2.transform.position.x < origin.x - threshold) {
+                Obj2.transform.position += new Vector3(x * Time.deltaTime, 0, 0);
+            } else if (Obj2.transform.position.x > origin.x && Obj2.transform.position.x > origin.x + threshold) {
+                Obj2.transform.position -= new Vector3(x * Time.deltaTime, 0, 0);
+            } else if (Obj2.transform.position.x == origin.x) {
+                return;
+            }
+            if (Obj2.transform.position.z < origin.z && Obj2.transform.position.z < origin.z - threshold) {
+                Obj2.transform.position += new Vector3(0, 0, z * Time.deltaTime);
+            } else if (Obj2.transform.position.z > origin.z && Obj2.transform.position.z > origin.z + threshold) {
+                Obj2.transform.position -= new Vector3(0, 0, z * Time.deltaTime);
+            } else if (Obj2.transform.position.z == origin.z) {
+                return;
+            }
+        }
+    }
+
+    private void AwayFromPlayer() {
+        if (!safeDistance) {
+            if (Obj2.transform.position.x > Obj1.transform.position.x) {
+                Obj2.transform.position += new Vector3(x * Time.deltaTime, 0, 0);
+            } else {
+                Obj2.transform.position -= new Vector3(x * Time.deltaTime, 0, 0);
+            }
+            if (Obj2.transform.position.z > Obj1.transform.position.z) {
+                Obj2.transform.position += new Vector3(0, 0, z * Time.deltaTime);
+            } else {
+                Obj2.transform.position -= new Vector3(0, 0, z * Time.deltaTime);
+            }
+        }
+    }
+}
