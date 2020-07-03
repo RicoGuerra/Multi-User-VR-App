6bb55e8ece3ede21f3e95be5fd5e4a8216592ae8
Enrico Guerra <engue002@fh-dortmund.de>
2020-07-03 22:45:47 +0200

    MenuScene - Tastatur fertig. Buchstaben den man auf der Tastatur tippt erscheint im InputField

---
+++ Assets/Scripts/Menu/KeyboardInput.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeyboardInput : MonoBehaviour {
+
+    public InputField Textfield;
+
+    private string myText;
+
+    public enum Example {
+        Text,
+        Button,
+        Nothing
+    };
+
+    public Example Options;
+
+    private void Start() {
+        myText = GetComponentInChildren<Text>().text;
+    }
+
+    public void MakeInput() {
+        if (myText == "DEL") {
+            Textfield.text = Textfield.text.Remove(Textfield.text.Length - 1);
+        } else {
+            Textfield.text += myText;
+        }
+    }
+}
