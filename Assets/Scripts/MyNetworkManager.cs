9334698631445009c7a33c78bd53a2b1c4dbfd58
Enrico Guerra <engue002@fh-dortmund.de>
2020-07-09 16:19:45 +0200

    Network buttons in menuScene impemented

--- Assets/Scripts/MyNetworkManager.cs
+++ Assets/Scripts/MyNetworkManager.cs
@@ -1,5 +1,34 @@
-﻿using UnityEngine.Networking;
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.Networking.Match;
 
 public class MyNetworkManager : NetworkManager {
 
+    private MatchInfoSnapshot match;
+
+    public void StartMatch() {
+        StartMatchMaker();
+        matchMaker.CreateMatch(matchName, matchSize, true, "", "", "", 0, 0, OnMatchCreate);
+    }
+
+    public override void OnMatchList(bool success, string extendedInfo, List<MatchInfoSnapshot> matchList) {
+        matches = matchList;
+        base.OnMatchList(success, extendedInfo, matchList);
+    }
+
+    public void JoinMatch() {
+        StartMatchMaker();
+        matchMaker.ListMatches(0, 1, "", true, 0, 0, OnMatchList);
+        foreach (MatchInfoSnapshot m in matches) {
+            if (m.name == matchName) {
+                match = m;
+            }
+        }
+        matchMaker.JoinMatch(match.networkId, "", "", "", 0, 0, OnMatchJoined);
+    }
+
+    public void EndGame() {
+        Application.Quit();
+    }
 }
