0ede0c0897604ac52e034d911f9553acb1ae838a
Enrico Guerra <engue002@fh-dortmund.de>
2020-07-22 23:04:35 +0200

    RoomTransition implemented. To be tested

--- Assets/Scripts/MainScene/GameManager.cs
+++ Assets/Scripts/MainScene/GameManager.cs
@@ -9,6 +9,8 @@ public class GameManager : MonoBehaviour {
     public GameObject[] players;
     public GameObject[] PassageWays;
 
+    private List<GameObject> playersInCorridor = new List<GameObject>();
+
     private void Update() {
         if (players.Length < 2) {
             players = GameObject.FindGameObjectsWithTag("Player");
@@ -33,18 +35,20 @@ public class GameManager : MonoBehaviour {
         PassageWays[corridorIndex].SetActive(false);
     }
 
-    public void RoomTransition(GameObject corridor) { 
-        // funktioniert nicht, da es dem Spieler nicht möglich ist Funktionen des GameManagers aufzurufen.
-        // Entweder muss der GameManager bzw das Skript ein Singleton sein, welches global verfügbar ist, oder die Kollisionserkennung muss von den Korridoren ausgehen
-        if (players[0].GetComponent<CheckTargetCollision>().TargetCollisionEnter && players[1].GetComponent<CheckTargetCollision>().TargetCollisionEnter) {
-            if (corridor == PassageWays[0]) {
-                WakeUpRoom(rooms[1]);
-                DeactivateRoom(rooms[0]);
-            } else if(corridor == PassageWays[1]) {
-                WakeUpRoom(rooms[2]);
-                DeactivateRoom(rooms[1]);
+    public void RoomTransition(GameObject corridor) {
+        GameObject playerObject = corridor.GetComponentInChildren<CheckTargetCollision>().TargetObjectInfo;
+        if (!playersInCorridor.Contains(playerObject)) {
+            playersInCorridor.Add(playerObject);
+        } else {
+            return;
+        }
+        for (int i = 0; i < PassageWays.Length; i++) {
+            if (corridor == PassageWays[i] && playersInCorridor.Count == 2) {
+                WakeUpRoom(rooms[i + 1]);
+                DeactivateRoom(rooms[i]);
+                playersInCorridor.Clear();
             } else {
-                Debug.LogError("Colliding object does not match any existing corridor in the scene!");
+                Debug.Log("Colliding object does not match any existing corridor in the scene or there are not exactly 2 different objects in the list.");
             }
         }
     }
