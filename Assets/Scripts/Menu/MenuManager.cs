20e608ec93395715f6c43922b016a8c8719dd80f
Enrico Guerra <engue002@fh-dortmund.de>
2020-08-10 12:49:04 +0200

    Added MenuManager and set up VR/NonVR input

---
+++ Assets/Scripts/Menu/MenuManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.XR;
+
+namespace Assets.Scripts.Menu {
+    // Managing properties of the menuScene
+    public class MenuManager : MonoBehaviour {
+
+        [SerializeField] private Canvas[] _menuCanvas;
+        [SerializeField] private GameObject _keyboard;
+
+        public Camera VRInputSource;
+        public Camera NonVRInputSource;
+
+        private void Start() {
+            SetupCanvasInput();
+        }
+
+        private void SetupCanvasInput() {
+            if (XRDevice.isPresent) {
+                foreach (Canvas c in _menuCanvas) {
+                    c.worldCamera = VRInputSource;
+                }
+                _keyboard.SetActive(true);
+            } else {
+                foreach (Canvas c in _menuCanvas) {
+                    c.worldCamera = NonVRInputSource;
+                }
+            }
+        }
+    }
+}
--- Assets/Scripts/MyNetworkManager.cs
+++ Assets/Scripts/MyNetworkManager.cs
@@ -47,4 +47,5 @@ public class MyNetworkManager : NetworkManager {
     public void EndGame() {
         Application.Quit();
     }
+
 }
